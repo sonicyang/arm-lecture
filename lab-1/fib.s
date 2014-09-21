e5d6618ac7a47a3153b678b75cf0819c3e626d98
sonicyang <sonic.tw.tp@gmail.com>
2014-09-21 19:52:23 +0800

    implempted according arm infocenter code

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -12,17 +12,17 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-
+	
 	@ R4 = R0 - 0 (update flags)
 	subs r4, r0, #0
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-	ble .L3	
+	ble .L3
 
 	@ Compare R4 wtih 1
 	cmp r4, 1
 	@ If R4 == 1 goto .L4 (which returns 1)
 	beq .L4
-
+	
 	@ R0 = R4 - 1(Setting function parameter as R4 - 1)
 	sub r0, r4, #1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -45,7 +45,6 @@ fibonacci:
 .L3:
 	mov r0, #0			@ R0 = 0
 	pop {r3, r4, r5, pc}		@ EPILOG
-
 .L4:
 	mov r0, #1			@ R0 = 1
 	pop {r3, r4, r5, pc}		@ EPILOG
--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,22 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
+	mov r1, #1
 
+.retry:
+	ldrex r2, [r0]
+
+.checklocked:
+	cmp r2, r1 
+	beq .checklocked
+	
+	strex r2, r1, [r0]
+
+	cmp r2, #1
+	beq .retry
+
+	dmb
+		
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +34,10 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+	mov r1, #0
+        dmb
+	str r1, [r0]
+
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
